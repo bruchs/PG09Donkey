cb904d449d30d8674cf244c7596595259ad5957e
pg09matias <pg09matias@PG104>
2016-12-01 17:21:34 -0800

    Start the donkey game! ;D

---
+++ PG09Snake/Board.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG09Snake
+{
+    class Board
+    {
+        private string m_sGameTitle = "Donkey's Tale"; 
+
+        public Board() { }
+
+        public void drawScreen()
+        {
+            Console.CursorVisible = (false);
+            Console.Title = "Snaaaaake!";
+            Console.SetWindowSize(56, 38);
+
+            Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+        }
+    }
+}
---
+++ PG09Snake/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG09Snake
+{
+    class Program
+    {
+        private static bool m_bExit;
+        static void Main(string[] args)
+        {
+            while (m_bExit != true)
+            {
+                Board myGameBoard = new Board();
+                myGameBoard.drawScreen();
+            }
+        }
+    }
+}
